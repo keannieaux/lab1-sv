7e38c8dbd86b07c6470d19a9912111ca5f9316ef
Khutsaidze Lizaveta <keannieaux@gmail.com>
2024-09-20 01:30:59 +0300

    feat: add component slider

--- react-lab1/src/Components/MainComponent/MainContent.jsx
+++ react-lab1/src/Components/MainComponent/MainContent.jsx
@@ -1,25 +1,65 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import SliderBlock from '../SliderComponent/SliderBlock';
+import './MainContent.css';
+import image1 from './image/pc.svg';
+import image2 from './image/pc2.svg';
+import image3 from './image/pc3.svg';
 
-class MainContent extends Component{
-    render(){
-        return(
+class MainContent extends Component {
+    render() {
+        const sliderData = [
+            {
+                id: 1,
+                image: image1, 
+                beginnerLevel: 'Beginner',
+                courseTitle: 'Webflow Basics',
+                price: '$ 24.00 USD',
+                buttonText: 'Enroll Now'
+            },
+            {
+                id: 2,
+                image: image2,
+                beginnerLevel: 'Intermediate',
+                courseTitle: 'Advanced CSS',
+                price: '$ 34.00 USD',
+                buttonText: 'Join Now'
+            },
+            {
+                id: 3,
+                image: image3,
+                beginnerLevel: 'Advanced',
+                courseTitle: 'React Mastery',
+                price: '$ 49.00 USD',
+                buttonText: 'Get Started'
+            }
+        ];
+
+        return (
             <main>
-            <div className="block-trending">
-              <div className="block-trending1">
-                <div className="block-trending2">
-                  <div className="text-trending">
-                    <p className="dm-sansFont text-trending1">Trending</p>
-                  </div>
-                  <p className="poppinsFont text-trending2">The Most Popular Courses</p>
-                  <p className="dm-sansFont text-trending3">Lorem ipsum dolor sit amet</p>
+                <div className="block-trending">
+                    <div className="block-trending1">
+                        <div className="block-trending2">
+                            <div className="text-trending">
+                                <p className="dm-sansFont text-trending1">Trending</p>
+                            </div>
+                            <p className="poppinsFont text-trending2">The Most Popular Courses</p>
+                            <p className="dm-sansFont text-trending3">Lorem ipsum dolor sit amet</p>
+                        </div>
+                        <div className="slider-container">
+                            {sliderData.map(data => (
+                                <SliderBlock
+                                    key={data.id}
+                                    image={data.image}
+                                    beginnerLevel={data.beginnerLevel}
+                                    courseTitle={data.courseTitle}
+                                    price={data.price}
+                                    buttonText={data.buttonText}
+                                />
+                            ))}
+                        </div>
+                    </div>
                 </div>
-                <div>
-            <SliderBlock />
-          </div>
-              </div>
-            </div>
-          </main>
+            </main>
         );
     }
 }
--- react-lab1/src/Components/SliderComponent/SliderBlock.jsx
+++ react-lab1/src/Components/SliderComponent/SliderBlock.jsx
@@ -1,29 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './SliderBlock.css';
-import pcIcon from './image/pc.svg';
 
-class SliderBlock extends Component{
-    render(){
-        return(
-                <div className="img-trending">
-                  <div className="img-pc">
-                  <div className="shopping_cart">
-                  <img src={pcIcon} alt="pc" />
-                  </div>
-                    <div className="text-img-pc">
-                      <div className="text-block-pc">
-                        <div className="button-img-pc">
-                          <p className="dm-sansFont text-img-pc1">Beginner</p>
-                        </div>
-                        <p className="poppinsFont text-img-pc2">Webflow Basics</p>
-                        <p className="dm-sansFont text-img-pc3">$ 24.00 USD</p>
-                      </div>
-                      <div className="button-img-pc1">
-                        <p className="dm-sansFont text-img-pc4">Enroll Now</p>
-                      </div>
+class SliderBlock extends Component {
+    render() {
+        const { image, beginnerLevel, courseTitle, price, buttonText } = this.props;
+
+        return (
+            <div className="img-trending">
+                <div className="img-pc">
+                    <div className="shopping_cart">
+                        <img src={image} alt="course" />
                     </div>
-                  </div>
+                    <div className="text-img-pc">
+                        <div className="text-block-pc">
+                            <div className="button-img-pc">
+                                <p className="dm-sansFont text-img-pc1">{beginnerLevel}</p>
+                            </div>
+                            <p className="poppinsFont text-img-pc2">{courseTitle}</p>
+                            <p className="dm-sansFont text-img-pc3">{price}</p>
+                        </div>
+                        <div className="button-img-pc1">
+                            <p className="dm-sansFont text-img-pc4">{buttonText}</p>
+                        </div>
+                    </div>
                 </div>
+            </div>
         );
     }
 }
